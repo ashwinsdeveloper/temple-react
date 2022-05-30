c3728dba45c495c880e876b32e3c66d917c74801
sarangan_E <sarangan@smitiv.co>
2022-05-30 17:53:30 +0530

    Basic Setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,8 @@
 import Head from "next/head";
 import '../styles/globals.css'
-// add bootstrap css 
-import "bootstrap/dist/css/bootstrap.css";
 import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  
-  useEffect(() => {
-    import("bootstrap/dist/js/bootstrap");
-  }, []);
 
   return <Component {...pageProps} />
   
